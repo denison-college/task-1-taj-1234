a096a019ce056a5afcd3455ec59961245ab422b2
taj-1234 <taj.richards@education.nsw.gov.au>
2026-03-09 12:43:58 +1100

    Catch up

---
+++ 1. Requirements/connect 4 requirements.md
@@ -0,0 +1,12 @@
+# Connect Four
+
+give them to play or get instructions
+
+diplay board
+
+give them the ability to put ther thing in a colum
+
+switch to dif players
+
+detect a win condition
+
---
+++ 3. Code and Tests/test.py
@@ -0,0 +1,16 @@
+ROWS = 6
+COLS = 7
+
+def create_board():
+    return [[" " for _ in range(COLS)] for _ in range(ROWS)]
+
+def print_board(board):
+    print("\n  " + "   ".join(str(i) for i in range(COLS)))
+
+    for row in board:
+        print(" | " + " | ".join(row) + " |")
+
+    print()
+board = create_board()
+
+print_board(board)
